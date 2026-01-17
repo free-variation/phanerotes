ef009c6f23efacccd18e99444f0de794088aa2b0
John Stewart <cane.cubo@gmail.com>
2026-01-17 14:30:08 -0500

    added image transposition and reflections

---
+++ example/image_functions.f90
@@ -0,0 +1,20 @@
+program load_unload
+    use :: stb_bindings
+    use :: image_transformations
+
+    real, allocatable :: pixels(:, :, :)
+    logical :: success
+    real, allocatable :: transformed_pixels(:, :, :)
+
+    pixels = load_image("images/test1.jpg")
+    print *, size(pixels, 2), size(pixels, 3), size(pixels, 1)
+
+    transformed_pixels = transpose_image(pixels)
+    call save_image("images/test1_transposed.png", transformed_pixels, success)
+    print *, success
+    
+    transformed_pixels = flip_image_vertical(pixels)
+    call save_image("images/test1_flipped_v.png", transformed_pixels, success)
+    print *, success
+
+end program
--- src/fortran/image_transformations.f90
+++ src/fortran/image_transformations.f90
@@ -1,13 +1,39 @@
 module image_transformations
-    intrinsic none
+    implicit none
 
+    contains
+        pure function transpose_image(pixels) result(pixels_out)
+            real, intent(in) :: pixels(:, :, :)
+            real, allocatable :: pixels_out(:, :, :)
+
+            integer :: channels, width, height
+
+            channels = size(pixels, 1)
+            width = size(pixels, 2)
+            height = size(pixels, 3)
+
+            pixels_out = reshape(pixels, shape = [channels, height, width], order = [1, 3, 2])
+        end function
+
+        pure function flip_image_horizontal(pixels) result(pixels_out)
+            real, intent(in) :: pixels(:, :, :)
+            real, allocatable :: pixels_out(:, :, :)
+
+            integer :: width
+
+            width = size(pixels, 2)
+            pixels_out = pixels(:, width:1:-1, :)
+        end function
+    
+        pure function flip_image_vertical(pixels) result(pixels_out)
+            real, intent(in) :: pixels(:, :, :)
+            real, allocatable :: pixels_out(:, :, :)
+
+            integer :: height
+
+            height = size(pixels, 3)
+            pixels_out = pixels(:, :, height:1:-1)
+        end function
 
 end module
-
-
-
-
-
-
-
 
